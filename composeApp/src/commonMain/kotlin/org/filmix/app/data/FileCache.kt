d2c61a14f25b33741fd4b535b36c6be0211c0187
nealt <193158093+nealt1@users.noreply.github.com>
2024-12-30 17:31:33 +0100

    don't keep in memory responses

--- composeApp/src/commonMain/kotlin/org/filmix/app/data/FileCache.kt
+++ composeApp/src/commonMain/kotlin/org/filmix/app/data/FileCache.kt
@@ -11,7 +11,7 @@ import kotlinx.io.writeString
 import kotlin.time.Duration.Companion.seconds
 
 class FileCache(private val cacheDir: String) {
-    private val cache = Cache.Builder<String, String>()
+    private val cache = Cache.Builder<String, Unit>()
         .expireAfterWrite(300.seconds)
         .eventListener { event ->
             println("onEvent: $event")
@@ -19,26 +19,29 @@ class FileCache(private val cacheDir: String) {
         .build()
 
     @OptIn(ExperimentalStdlibApi::class)
-    suspend fun getOrPut(key: String, provider: suspend () -> String): String {
-        return cache.get(key) {
-            val fileNameHash = key.toByteArray().sha256().hexLower
-            val folderPath = Path(cacheDir, fileNameHash.take(2))
+    suspend fun getOrPut(url: String, provider: suspend () -> String): String {
+        val hash = getUrlHash(url)
+        val folderPath = Path(cacheDir, hash.take(2))
+        val filePath = Path(folderPath, hash)
+
+        cache.get(hash) {
             SystemFileSystem.createDirectories(folderPath)
-            val filePath = Path(folderPath, fileNameHash)
 
             try {
-                provider().also { data ->
-                    SystemFileSystem.sink(filePath).buffered().use { it.writeString(data) }
-                }
+                val data = provider()
+                SystemFileSystem.sink(filePath).buffered().use { it.writeString(data) }
             } catch (e: Exception) {
                 println("FileCache: failed to invalidate cache")
-                if (!SystemFileSystem.exists(filePath)) throw e
-                SystemFileSystem.source(filePath).buffered().use { it.readString() }
             }
         }
+
+        return SystemFileSystem.source(filePath).buffered().use { it.readString() }
     }
 
-    fun exists(key: String): Boolean {
-        return cache.get(key) != null
+    fun exists(url: String): Boolean {
+        val hash = getUrlHash(url)
+        return cache.get(hash) != null
     }
+
+    private fun getUrlHash(url: String) = url.toByteArray().sha256().hexLower
 }
