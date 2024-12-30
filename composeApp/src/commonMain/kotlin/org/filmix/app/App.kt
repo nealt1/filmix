6c1fa335f3a20d21364b5dfa7c0c34a9814dec82
nealt <193158093+nealt1@users.noreply.github.com>
2024-12-30 17:31:36 +0100

    update theme

--- composeApp/src/commonMain/kotlin/org/filmix/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/filmix/app/App.kt
@@ -1,18 +1,17 @@
 package org.filmix.app
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,7 +20,6 @@ import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.TabDisposable
 import cafe.adriel.voyager.navigator.tab.TabNavigator
 import org.filmix.app.app.AppState
-import org.filmix.app.app.AppTheme
 import org.filmix.app.components.LoadingIndicator
 import org.filmix.app.screens.navigation.BottomNavigationBar
 import org.filmix.app.screens.navigation.SideNavigationBar
@@ -31,6 +29,7 @@ import org.filmix.app.tabs.SearchTab
 import org.filmix.app.tabs.SettingsTab
 import org.filmix.app.ui.ProvideUserInfo
 import org.filmix.app.ui.ProvideWindowSizeClass
+import org.filmix.app.ui.theme.ApplicationTheme
 import org.koin.compose.getKoin
 
 @Composable
@@ -38,21 +37,8 @@ fun App() {
     val koin = getKoin()
     val state = remember { koin.get<AppState>() }
     val userProfile by state.userProfile.collectAsState()
-    val isSystemInDarkTheme = isSystemInDarkTheme()
-    val colorScheme = remember {
-        derivedStateOf {
-            when (state.theme) {
-                AppTheme.AUTO -> if (isSystemInDarkTheme) {
-                    darkColorScheme()
-                } else lightColorScheme()
 
-                AppTheme.DARK -> darkColorScheme()
-                AppTheme.LIGHT -> lightColorScheme()
-            }
-        }
-    }
-
-    MaterialTheme(colorScheme.value) {
+    ApplicationTheme {
         ProvideWindowSizeClass {
             LoadingIndicator(userProfile, loading = {
                 Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
