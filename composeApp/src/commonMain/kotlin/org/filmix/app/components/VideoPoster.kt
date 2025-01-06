c42d1662b4db99a3b1421e5659786c9180bfd718
nealt <193158093+nealt1@users.noreply.github.com>
2025-01-06 14:02:17 +0100

    fix poster loading

--- composeApp/src/commonMain/kotlin/org/filmix/app/components/VideoPoster.kt
+++ composeApp/src/commonMain/kotlin/org/filmix/app/components/VideoPoster.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.kamel.image.KamelImage
@@ -16,15 +15,14 @@ private val log = logging()
 
 @Composable
 fun VideoPoster(video: Video) {
-    val posterUrl = remember { video.poster.replace("http://", "https://") }
-    val posterResource = asyncPainterResource(data = posterUrl)
+    val posterResource = asyncPainterResource(data = video.poster)
     KamelImage(
         resource = posterResource,
         contentDescription = video.title,
         onLoading = { progress ->
             CircularProgressIndicator({ progress })
         },
-        onFailure = { log.error(it) { "Failed to load image $posterUrl" } },
+        onFailure = { log.error(it) { "Failed to load image ${video.poster}" } },
         modifier = Modifier.height(313.dp).width(220.dp)
     )
 }
